b419e177926639112d29c40e58ef8da5a5f25e6c
mauricio <maur28363429rd@gmail.com>
2022-06-01 09:28:45 -0400

    version estable

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+
+  define( 'DB_HOST', 'localhost' );          // Definir  host de la base de datos
+  define( 'DB_USER', 'root' );             // Definir  user de la base de datos
+  define( 'DB_PASS', '' );             // Definir  password de la base de datos
+  define( 'DB_NAME', 'gustavo' );        // Definir  name de la base de datos
+
+?>
---
+++ layouts/footer.php
@@ -0,0 +1,10 @@
+     </div>
+    </div>
+    <script src="libs/js/jquery.min.js"></script>
+  <script src="libs/js/bootstrap.min.js"></script>
+  <script src=""></script>
+  <script type="text/javascript" src="libs/js/functions.js"></script>
+  </body>
+</html>
+
+<?php if(isset($db)) { $db->db_disconnect(); } ?>
